200eef00ee1c9b9a7b2010136a90b8c0ba1fe27e
Annarhysa Albert <annarhysa13@gmail.com>
2023-08-03 23:02:24 +0530

    changes in languages for  code input

--- constants/languages.js
+++ constants/languages.js
@@ -8,41 +8,41 @@ export const languages = [
   { value: 'Vue', label: 'Vue' }, 
   { value: 'Go', label: 'Go' }, 
   { value: 'C', label: 'C' }, 
-  { value: 'C++', label: 'C++' }, // C++
-  { value: 'Java', label: 'Java' }, // Java
-  { value: 'C#', label: 'C#' }, // C#
-  { value: 'Visual Basic .NET', label: 'Visual Basic .NET' }, // Visual Basic .NET
-  { value: 'SQL', label: 'SQL' }, // SQL
-  { value: 'Assembly Language', label: 'Assembly Language' }, // Assembly Language
-  { value: 'PHP', label: 'PHP' }, // PHP
-  { value: 'Ruby', label: 'Ruby' }, // Ruby
-  { value: 'Swift', label: 'Swift' }, // Swift
-  { value: 'SwiftUI', label: 'SwiftUI' }, // SwiftUI
-  { value: 'Kotlin', label: 'Kotlin' }, // Kotlin
-  { value: 'R', label: 'R' }, // R
-  { value: 'Objective-C', label: 'Objective-C' }, // Objective-C
-  { value: 'Perl', label: 'Perl' }, // Perl
-  { value: 'SAS', label: 'SAS' }, // SAS
-  { value: 'Scala', label: 'Scala' }, // Scala
-  { value: 'Dart', label: 'Dart' }, // Dart
-  { value: 'Rust', label: 'Rust' }, // Rust
-  { value: 'Haskell', label: 'Haskell' }, // Haskell
-  { value: 'Lua', label: 'Lua' }, // Lua
-  { value: 'Groovy', label: 'Groovy' }, // Groovy
-  { value: 'Elixir', label: 'Elixir' }, // Elixir
-  { value: 'Clojure', label: 'Clojure' }, // Clojure
-  { value: 'Lisp', label: 'Lisp' }, // Lisp
-  { value: 'Julia', label: 'Julia' }, // Julia
-  { value: 'Matlab', label: 'Matlab' }, // Matlab
-  { value: 'Fortran', label: 'Fortran' }, // Fortran
-  { value: 'COBOL', label: 'COBOL' }, // COBOL
-  { value: 'Bash', label: 'Bash' }, // Bash
-  { value: 'Powershell', label: 'Powershell' }, // Powershell
-  { value: 'PL/SQL', label: 'PL/SQL' }, // PL/SQL
-  { value: 'CSS', label: 'CSS' }, // CSS
-  { value: 'Racket', label: 'Racket' }, // Racket
-  { value: 'HTML', label: 'HTML' }, // HTML
-  { value: 'NoSQL', label: 'NoSQL' }, // NoSQL
-  { value: 'Natural Language', label: 'Natural Language' }, // Natural Language
-  { value: 'CoffeeScript', label: 'CoffeeScript' }, // CoffeeScript
+  { value: 'C++', label: 'C++' }, 
+  { value: 'Java', label: 'Java' },
+  { value: 'C#', label: 'C#' },
+  { value: 'Visual Basic .NET', label: 'Visual Basic .NET' }, 
+  { value: 'SQL', label: 'SQL' },
+  { value: 'Assembly Language', label: 'Assembly Language' },
+  { value: 'PHP', label: 'PHP' },
+  { value: 'Ruby', label: 'Ruby' }, 
+  { value: 'Swift', label: 'Swift' }, 
+  { value: 'SwiftUI', label: 'SwiftUI' }, 
+  { value: 'Kotlin', label: 'Kotlin' },
+  { value: 'R', label: 'R' }, 
+  { value: 'Objective-C', label: 'Objective-C' }, 
+  { value: 'Perl', label: 'Perl' }, 
+  { value: 'SAS', label: 'SAS' }, 
+  { value: 'Scala', label: 'Scala' }, 
+  { value: 'Dart', label: 'Dart' }, 
+  { value: 'Rust', label: 'Rust' }, 
+  { value: 'Haskell', label: 'Haskell' }, 
+  { value: 'Lua', label: 'Lua' },
+  { value: 'Groovy', label: 'Groovy' }, 
+  { value: 'Elixir', label: 'Elixir' }, 
+  { value: 'Clojure', label: 'Clojure' }, 
+  { value: 'Lisp', label: 'Lisp' }, 
+  { value: 'Julia', label: 'Julia' }, 
+  { value: 'Matlab', label: 'Matlab' }, 
+  { value: 'Fortran', label: 'Fortran' }, 
+  { value: 'COBOL', label: 'COBOL' }, 
+  { value: 'Bash', label: 'Bash' }, 
+  { value: 'Powershell', label: 'Powershell' }, 
+  { value: 'PL/SQL', label: 'PL/SQL' }, 
+  { value: 'CSS', label: 'CSS' }, 
+  { value: 'Racket', label: 'Racket' }, 
+  { value: 'HTML', label: 'HTML' }, 
+  { value: 'NoSQL', label: 'NoSQL' }, 
+  { value: 'Natural Language', label: 'Natural Language' }, 
+  { value: 'CoffeeScript', label: 'CoffeeScript' }, 
 ];
