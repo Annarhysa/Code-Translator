955ae0d8012fa19e7c3f53b8d4e1de317eabc7ac
Annarhysa Albert <annarhysa13@gmail.com>
2023-08-03 22:52:12 +0530

    Changes in Language Select file

--- components/LanguageSelect.js
+++ components/LanguageSelect.js
@@ -1,3 +1,23 @@
 // Import the `languages` object from the `@/constants/languages` module.
 import { languages } from '@/constants/languages';
+import Select from 'react-select';
 
+const LanguageSelect = ({ language, handleLanguageChange, disabled }) => {
+    const formatOptionLabel = ({ _, label }) => (
+        // Return a React component that displays the `label` value as a paragraph with a "Select" indicator if the `language` prop matches the `label`.
+        <div className='flex flex-row justify-between items-center'><p>{label}</p>{language===label && <p className='text-xs text-slate-400'>Select</p>}</div>
+    );
+    return (
+        <Select
+            className="w-full rounded-t-lg border-2 border-black"
+            value={{label: language, value: language}}
+            onChange={handleLanguageChange}
+            isDisabled={disabled}
+            options={languages.sort((a, b) => a.label.localeCompare(b.label))}
+            formatOptionLabel={formatOptionLabel}
+        >
+        </Select>
+    );
+};
+
+export default LanguageSelect;
