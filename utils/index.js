3b0a1c8db2b94e1a022db88d0dd5dbd284bf6144
Annarhysa Albert <annarhysa13@gmail.com>
2023-08-04 20:19:42 +0530

    changes in api index file

--- utils/index.js
+++ utils/index.js
@@ -4,63 +4,15 @@ const apiKey = process.env.OPENAI_API_KEY;
 
 const createPrompt = (inputLanguage, outputLanguage, inputCode) => {
   if (inputLanguage === 'Natural Language') {
-    return endent`
-    You are an expert programmer in all programming languages. Translate the natural language to "${outputLanguage}" code. Do not include \`\`\`.
-
-    Example translating from natural language to JavaScript:
-
-    Natural language:
-    Print the numbers 0 to 9.
-
-    JavaScript code:
-    for (let i = 0; i < 10; i++) {
-      console.log(i);
-    }
-
-    Natural language:
-    ${inputCode}
-
-    ${outputLanguage} code (no \`\`\`):
-    `;
-  } else if (outputLanguage === 'Natural Language') {
-    return endent`
-      You are an expert programmer in all programming languages. Translate the "${inputLanguage}" code to natural language in plain English that the average adult could understand. Respond as bullet points starting with -.
-
-      Example translating from JavaScript to natural language:
-
-      JavaScript code:
-      for (let i = 0; i < 10; i++) {
-        console.log(i);
-      }
-
-      Natural language:
-      Print the numbers 0 to 9.
-
-      ${inputLanguage} code:
-      ${inputCode}
-
-      Natural language:
-     `;
-  } else {
-    return endent`
-      You are an expert programmer in all programming languages. Translate the "${inputLanguage}" code to "${outputLanguage}" code. Do not include \`\`\`.
-
-      Example translating from JavaScript to Python:
-
-      JavaScript code:
-      for (let i = 0; i < 10; i++) {
-        console.log(i);
-      }
-
-      Python code:
-      for i in range(10):
-        print(i)
-
-      ${inputLanguage} code:
-      ${inputCode}
-
-      ${outputLanguage} code (no \`\`\`):
-     `;
+    return endent;
+  } 
+  
+  else if (outputLanguage === 'Natural Language') {
+    return endent;
+  } 
+  
+  else {
+    return endent;
   }
 };
 
