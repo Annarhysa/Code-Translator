2834f3590c8a4020c2c16a8f9214dc521b0e3d7c
Annarhysa Albert <annarhysa13@gmail.com>
2023-08-04 10:54:51 +0530

    changes in index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -18,5 +18,33 @@ export default fucntion Home(){
         setOutputCode('')
       }
     
-  
+    const handleOutputLanguageChange = (option) => {
+        setOutputLanguage(option.value)
+        setOutputCode('')
+      }
+
+    const handleTranslate = async () => {
+        const maxCodeLength = 6000;
+        if (inputLanguage === outputLanguage) {
+            alert('Please select different languages.');
+            return;
+          }
+
+        if (!inputCode) {
+            alert('Please enter some code.');
+            return;
+          }
+
+        if (inputCode.length > maxCodeLength) {
+            alert(
+              `Please enter code less than ${maxCodeLength} characters. You are currently at ${inputCode.length} characters.`,
+            );
+            return;
+          }
+        setLoading(true);
+        setOutputCode('');
+
+        
+          
+          
 }
